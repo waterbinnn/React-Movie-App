9ca44b15181c25c987322a34ac095742d90d848c
waterbinnn <ewaterbin@naver.com>
2022-11-20 03:39:31 +0900

    🚧 Working on Home components

--- movie-app/src/components/Home/Home.jsx
+++ movie-app/src/components/Home/Home.jsx
@@ -1,7 +1,34 @@
 import React from 'react';
+import { useEffect } from 'react';
+import MovieListing from '../MovieListing/MovieListing';
+import movieApi from '../../apis/movieApi';
+import { APIKey } from '../../apis/MovieApiKey';
+import { useDispatch } from 'react-redux';
+import { addMovies } from '../../features/movies/movieSlice';
 
 const Home = () => {
-  return <></>;
+  const movieText = 'Harry';
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const fetchMovies = async () => {
+      const res = await movieApi
+        .get(`/?apiKey=${APIKey}&s=${movieText}&type=movie`)
+        .catch((err) => {
+          console.error(err);
+        });
+      dispatch(addMovies(res.data));
+    };
+
+    fetchMovies();
+  }, []);
+
+  return (
+    <>
+      <div className="banner-img"></div>
+      <MovieListing />
+    </>
+  );
 };
 
 export default Home;
