818e3ef06fdfb41b62f6299206ef5b12d5d50050
waterbinnn <ewaterbin@naver.com>
2022-11-19 22:53:20 +0900

    🚧 Set initial state of components

---
+++ movie-app/src/components/Footer/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Footer.scss';
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <p>Movie App</p>
+      <span>@copyright waterbinnn</span>
+    </div>
+  );
+};
+
+export default Footer;
