56fd330ffc17028325da2db5106c309e66c7b05d
waterbinnn <ewaterbin@naver.com>
2022-11-20 16:04:37 +0900

    ♻️ redux toolkit 사용하여 패칭하도록 리팩토링

--- movie-app/src/components/Home/Home.jsx
+++ movie-app/src/components/Home/Home.jsx
@@ -1,27 +1,20 @@
 import React from 'react';
 import { useEffect } from 'react';
 import MovieListing from '../MovieListing/MovieListing';
-import movieApi from '../../apis/movieApi';
-import { APIKey } from '../../apis/MovieApiKey';
+
 import { useDispatch } from 'react-redux';
-import { addMovies } from '../../features/movies/movieSlice';
+import {
+  fetchAsyncMovies,
+  fetchAsyncShows,
+} from '../../features/movies/movieSlice';
 
 const Home = () => {
-  const movieText = 'Harry';
   const dispatch = useDispatch();
 
   useEffect(() => {
-    const fetchMovies = async () => {
-      const res = await movieApi
-        .get(`/?apiKey=${APIKey}&s=${movieText}&type=movie`)
-        .catch((err) => {
-          console.error(err);
-        });
-      dispatch(addMovies(res.data));
-    };
-
-    fetchMovies();
-  }, []);
+    dispatch(fetchAsyncMovies());
+    dispatch(fetchAsyncShows());
+  }, [dispatch]);
 
   return (
     <>
