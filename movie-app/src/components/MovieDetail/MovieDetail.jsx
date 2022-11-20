3a54baeb8c64f3d2971e82eef4cb7026d013d701
waterbinnn <ewaterbin@naver.com>
2022-11-20 16:58:04 +0900

    🔨 Add Loading state

--- movie-app/src/components/MovieDetail/MovieDetail.jsx
+++ movie-app/src/components/MovieDetail/MovieDetail.jsx
@@ -5,6 +5,7 @@ import { useDispatch, useSelector } from 'react-redux';
 import {
   fetchAsyncMovieOrShowDetail,
   getSelectedMovieOrShow,
+  removeSelectedMovieOrShow,
 } from '../../features/movies/movieSlice';
 
 const MovieDetail = () => {
@@ -14,50 +15,60 @@ const MovieDetail = () => {
   console.log(data);
   useEffect(() => {
     dispatch(fetchAsyncMovieOrShowDetail(imdbID));
+
+    return () => {
+      dispatch(removeSelectedMovieOrShow());
+    };
   }, [dispatch, imdbID]);
 
   return (
     <div className="movie-section">
-      <div className="section-left">
-        <div className="movie-title">{data.Title}</div>
-        <div className="movie-rating">
-          <span>
-            <i className="fa fa-star"></i> Rating_ {data.imdbRating}
-          </span>
-          <span>
-            <i className="fa fa-film"></i> Runtime_ {data.Runtime}
-          </span>
-          <span>
-            <i className="fa fa-calendar"></i> Year_ {data.Year}
-          </span>
-        </div>
-        <div className="movie-plot"></div>
-        <div className="movie-info">
-          <div>
-            <span>Director</span>
-            <span>{data.Director}</span>
+      {Object.keys(data).length === 0 ? (
+        <div>...Loading</div>
+      ) : (
+        <>
+          <div className="section-left">
+            <div className="movie-title">{data.Title}</div>
+            <div className="movie-rating">
+              <span>
+                <i className="fa fa-star"></i> Rating_ {data.imdbRating}
+              </span>
+              <span>
+                <i className="fa fa-film"></i> Runtime_ {data.Runtime}
+              </span>
+              <span>
+                <i className="fa fa-calendar"></i> Year_ {data.Year}
+              </span>
+            </div>
+            <div className="movie-plot">{data.Plot}</div>
+            <div className="movie-info">
+              <div>
+                <span>Director</span>
+                <span>{data.Director}</span>
+              </div>
+              <div>
+                <span>Stars</span>
+                <span>{data.Actors}</span>
+              </div>
+              <div>
+                <span>Genres</span>
+                <span>{data.Genre}</span>
+              </div>
+              <div>
+                <span>Language</span>
+                <span>{data.Language}</span>
+              </div>
+              <div>
+                <span>Awards</span>
+                <span>{data.Awards}</span>
+              </div>
+            </div>
           </div>
-          <div>
-            <span>Stars</span>
-            <span>{data.Actors}</span>
+          <div className="section-right">
+            <img src={data.Poster} alt={data.Title} />
           </div>
-          <div>
-            <span>Genres</span>
-            <span>{data.Genre}</span>
-          </div>
-          <div>
-            <span>Language</span>
-            <span>{data.Language}</span>
-          </div>
-          <div>
-            <span>Awards</span>
-            <span>{data.Awards}</span>
-          </div>
-        </div>
-      </div>
-      <div className="section-right">
-        <img src={data.Poster} alt={data.Title} />
-      </div>
+        </>
+      )}
     </div>
   );
 };
