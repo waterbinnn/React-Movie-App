666d9614c0cb260d849363b55c4d60feb4034806
waterbinnn <ewaterbin@naver.com>
2022-11-20 16:48:22 +0900

    🔨 영화 상세 페이지 구현

--- movie-app/src/components/MovieDetail/MovieDetail.jsx
+++ movie-app/src/components/MovieDetail/MovieDetail.jsx
@@ -1,7 +1,65 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import './MovieDetail.scss';
+import { useParams } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import {
+  fetchAsyncMovieOrShowDetail,
+  getSelectedMovieOrShow,
+} from '../../features/movies/movieSlice';
 
 const MovieDetail = () => {
-  return <div>MovieDetail</div>;
+  const { imdbID } = useParams();
+  const dispatch = useDispatch();
+  const data = useSelector(getSelectedMovieOrShow);
+  console.log(data);
+  useEffect(() => {
+    dispatch(fetchAsyncMovieOrShowDetail(imdbID));
+  }, [dispatch, imdbID]);
+
+  return (
+    <div className="movie-section">
+      <div className="section-left">
+        <div className="movie-title">{data.Title}</div>
+        <div className="movie-rating">
+          <span>
+            <i className="fa fa-star"></i> Rating_ {data.imdbRating}
+          </span>
+          <span>
+            <i className="fa fa-film"></i> Runtime_ {data.Runtime}
+          </span>
+          <span>
+            <i className="fa fa-calendar"></i> Year_ {data.Year}
+          </span>
+        </div>
+        <div className="movie-plot"></div>
+        <div className="movie-info">
+          <div>
+            <span>Director</span>
+            <span>{data.Director}</span>
+          </div>
+          <div>
+            <span>Stars</span>
+            <span>{data.Actors}</span>
+          </div>
+          <div>
+            <span>Genres</span>
+            <span>{data.Genre}</span>
+          </div>
+          <div>
+            <span>Language</span>
+            <span>{data.Language}</span>
+          </div>
+          <div>
+            <span>Awards</span>
+            <span>{data.Awards}</span>
+          </div>
+        </div>
+      </div>
+      <div className="section-right">
+        <img src={data.Poster} alt={data.Title} />
+      </div>
+    </div>
+  );
 };
 
 export default MovieDetail;
