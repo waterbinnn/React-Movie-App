2fa6a035ab7a3eed1962369e304f2dce445ac167
waterbinnn <ewaterbin@naver.com>
2022-11-20 16:49:42 +0900

    🔨 Movies, Shows detail fetch slice 작성

--- movie-app/src/features/movies/movieSlice.js
+++ movie-app/src/features/movies/movieSlice.js
@@ -2,64 +2,74 @@ import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import movieApi from '../../apis/movieApi';
 import { APIKey } from '../../apis/MovieApiKey';
 
-const initialState = {
-  movies: {},
-  series: {},
-};
-
 export const fetchAsyncMovies = createAsyncThunk(
   'movies/fetchAsyncMovies',
-
   async () => {
     const movieText = 'Harry';
-    const res = await movieApi.get(
-      `/?apiKey=${APIKey}&s=${movieText}&type=movie`
+    const response = await movieApi.get(
+      `?apiKey=${APIKey}&s=${movieText}&type=movie`
     );
-    console.log(res);
-    return res.data;
+    return response.data;
   }
 );
+
 export const fetchAsyncShows = createAsyncThunk(
   'movies/fetchAsyncShows',
-
   async () => {
     const seriesText = 'Friends';
-    const res = await movieApi.get(
-      `/?apiKey=${APIKey}&s=${seriesText}&type=series`
+    const response = await movieApi.get(
+      `?apiKey=${APIKey}&s=${seriesText}&type=series`
     );
-    console.log(res);
-    return res.data;
+    return response.data;
   }
 );
+
+export const fetchAsyncMovieOrShowDetail = createAsyncThunk(
+  'movies/fetchAsyncMovieOrShowDetail',
+  async (id) => {
+    const response = await movieApi.get(`?apiKey=${APIKey}&i=${id}&Plot=full`);
+    return response.data;
+  }
+);
+
+const initialState = {
+  movies: {},
+  shows: {},
+  selectMovieOrShow: {},
+};
 
 const movieSlice = createSlice({
   name: 'movies',
   initialState,
   reducers: {
-    addMovies: (state, { payload }) => {
-      state.movies = payload;
+    removeSelectedMovieOrShow: (state) => {
+      state.selectMovieOrShow = {};
     },
   },
-
-  extraReducers: (builder) => {
-    builder.addCase(fetchAsyncMovies.pending, () => {
+  extraReducers: {
+    [fetchAsyncMovies.pending]: () => {
       console.log('Pending');
-    });
-    builder.addCase(fetchAsyncMovies.fulfilled, (state, { payload }) => {
-      console.log('Fetched');
+    },
+    [fetchAsyncMovies.fulfilled]: (state, { payload }) => {
+      console.log('Fetched Successfully!');
       return { ...state, movies: payload };
-    });
-    builder.addCase(fetchAsyncMovies.rejected, () => {
-      console.log('Rejected');
-    });
-    builder.addCase(fetchAsyncShows.fulfilled, (state, { payload }) => {
-      console.log('Fetched');
+    },
+    [fetchAsyncMovies.rejected]: () => {
+      console.log('Rejected!');
+    },
+    [fetchAsyncShows.fulfilled]: (state, { payload }) => {
+      console.log('Fetched Successfully!');
       return { ...state, shows: payload };
-    });
+    },
+    [fetchAsyncMovieOrShowDetail.fulfilled]: (state, { payload }) => {
+      console.log('Fetched Successfully!');
+      return { ...state, selectMovieOrShow: payload };
+    },
   },
 });
 
-export const { addMovies } = movieSlice.actions;
+export const { removeSelectedMovieOrShow } = movieSlice.actions;
 export const getAllMovies = (state) => state.movies.movies;
 export const getAllShows = (state) => state.movies.shows;
+export const getSelectedMovieOrShow = (state) => state.movies.selectMovieOrShow;
 export default movieSlice.reducer;
