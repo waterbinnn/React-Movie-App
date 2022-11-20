c4d872ad2587df31a9aaa3ec692af28dd80a0856
waterbinnn <ewaterbin@naver.com>
2022-11-20 16:47:08 +0900

    💄 title icon 제거, font weight, color 수정

--- movie-app/src/components/MovieListing/MovieListing.jsx
+++ movie-app/src/components/MovieListing/MovieListing.jsx
@@ -34,11 +34,11 @@ const MovieListing = () => {
   return (
     <div className="movie-wrapper">
       <div className="movie-list">
-        <h2>🎬 MOVIES</h2>
+        <h2>MOVIES</h2>
         <div className="movie-container">{renderMovies}</div>
       </div>
       <div className="show-list">
-        <h2>🍔 SHOWS</h2>
+        <h2>SHOWS</h2>
         <div className="movie-container">{renderShows}</div>
       </div>
     </div>
