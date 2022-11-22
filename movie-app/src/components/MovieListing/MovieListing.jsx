233f9b4f343680272c8f5df47365d761b3cedf2f
waterbinnn <ewaterbin@naver.com>
2022-11-23 02:50:45 +0900

    🔨 Error, Loading 처리

--- movie-app/src/components/MovieListing/MovieListing.jsx
+++ movie-app/src/components/MovieListing/MovieListing.jsx
@@ -1,11 +1,10 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import Slider from 'react-slick';
-import { useDispatch, useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import {
-  fetchAsyncMovies,
   getAllMovies,
   getAllShows,
-  getTerm,
+  removeMovieOrShow,
 } from '../../features/movies/movieSlice';
 import MovieCard from '../MovieCard/MovieCard';
 import { Settings } from '../../common/settings';
@@ -14,6 +13,10 @@ import './MovieListing.scss';
 const MovieListing = () => {
   const movies = useSelector(getAllMovies);
   const shows = useSelector(getAllShows);
+
+  const dispatch = useDispatch();
+
+  const term = useSelector((state) => state.movies.term);
 
   let renderMovies,
     renderShows = '';
@@ -25,7 +28,7 @@ const MovieListing = () => {
       ))
     ) : (
       <div className="movies-error">
-        <h3>{movies.Error}</h3>
+        <h3>Sorry can't find that movie</h3>
       </div>
     );
 
@@ -34,24 +37,40 @@ const MovieListing = () => {
       shows.Search.map((movie, index) => <MovieCard key={index} data={movie} />)
     ) : (
       <div className="shows-error">
-        <h3>{shows.Error}</h3>
+        <h3>Sorry can't find that show</h3>
       </div>
     );
 
+  useEffect(() => {
+    return () => {
+      dispatch(removeMovieOrShow());
+    };
+  }, []);
+
   return (
     <div className="movie-wrapper">
-      <div className="movie-list">
-        <h2>MOVIES</h2>
-        <div className="movie-container">
-          <Slider {...Settings}>{renderMovies}</Slider>
+      <>
+        <div className="movie-list">
+          <h2>MOVIES</h2>
+          {movies.length === 0 ? (
+            <div>Loading...</div>
+          ) : (
+            <div className="movie-container">
+              <Slider {...Settings}>{renderMovies}</Slider>
+            </div>
+          )}
         </div>
-      </div>
-      <div className="show-list">
-        <h2>SHOWS</h2>
-        <div className="movie-container">
-          <Slider {...Settings}>{renderShows}</Slider>
+        <div className="show-list">
+          <h2>SHOWS</h2>
+          {shows.length === 0 ? (
+            <div>Loading...</div>
+          ) : (
+            <div className="movie-container">
+              <Slider {...Settings}>{renderShows}</Slider>
+            </div>
+          )}
         </div>
-      </div>
+      </>
     </div>
   );
 };
