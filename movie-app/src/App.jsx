e8ffb1ec1a1d767379b90134e2223089e408f730
waterbinnn <ewaterbin@naver.com>
2022-11-23 02:45:42 +0900

    ♻️ Add MovieListing Route

--- movie-app/src/App.jsx
+++ movie-app/src/App.jsx
@@ -5,6 +5,7 @@ import './App.scss';
 import Header from './components/Header/Header';
 import Home from './components/Home/Home';
 import MovieDetail from './components/MovieDetail/MovieDetail';
+import MovieListing from './components/MovieListing/MovieListing';
 import PageNotFound from './components/PageNotfound/PageNotFound';
 
 function App() {
@@ -15,6 +16,7 @@ function App() {
         <div className="container">
           <Routes>
             <Route path="/" element={<Home />} />
+            <Route path="/:term" element={<MovieListing />} />
             <Route path="/movie/:imdbID" element={<MovieDetail />} />
             <Route element={<PageNotFound />} />
           </Routes>
