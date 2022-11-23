2023d969cf691fe156e08c6805ac91ecaac574b1
waterbinnn <ewaterbin@naver.com>
2022-11-23 16:28:38 +0900

    ➕ 데이터 유지위한 persistReducer 추가

--- movie-app/src/features/store.js
+++ movie-app/src/features/store.js
@@ -1,8 +1,24 @@
 import { configureStore } from '@reduxjs/toolkit';
+import storage from 'redux-persist/lib/storage';
+import { combineReducers } from 'redux';
+import { persistReducer } from 'redux-persist';
 import movieReducer from './movies/movieSlice';
+
+const persistConfig = {
+  key: 'root',
+  storage,
+};
+
+const reducers = combineReducers({
+  movies: movieReducer,
+});
+
+const persistedReducer = persistReducer(persistConfig, reducers);
 
 export const store = configureStore({
   reducer: {
     movies: movieReducer,
+    persistedReducer,
   },
+  devTools: process.env.NODE_ENV !== 'production',
 });
